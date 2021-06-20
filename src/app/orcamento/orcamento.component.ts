b2410bfcb024490cc584475db6091636681b98e9
Paulo <pauloh2012sul@gmail.compwd>
2021-06-20 20:16:46 -0300

    perfil

--- src/app/orcamento/orcamento.component.ts
+++ src/app/orcamento/orcamento.component.ts
@@ -1,15 +1,58 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-orcamento',
   templateUrl: './orcamento.component.html',
   styleUrls: ['./orcamento.component.css']
 })
+
+
 export class OrcamentoComponent implements OnInit {
 
-  constructor() { }
+  orcamento: FormGroup;
+  loading = false;
+  submitted = false;
 
-  ngOnInit(): void {
+  marcas = [
+    {
+    id: 1,
+    nome: 'Nissan'
+  },
+  {
+    id: 2,
+    nome: 'Renault'
+  },
+]
+
+veiculos = [
+  {
+  id: 1,
+  nome: '350Z'
+},
+{
+  id: 2,
+  nome: 'Supra'
+},
+]
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+
+    this.orcamento = this.formBuilder.group({
+      marca: ['', Validators.required],
+      veiculo: ['', Validators.required],
+      //cidade: ['', Validators.required],     
+      //estado: ['', Validators.required],    
+      //uf: ['', Validators.required],    
+    });
+
+  }
+
+  onSubmit() {
+
+
   }
 
 }
